d88542bbdf48502b3f61ddc145e94276be39c566
shivaprasadks <shivaprasad.kssp@gmail.com>
2017-02-24 14:52:50 +0530

    Transition for the bar chart and fetching 2 different json

--- app.js
+++ app.js
@@ -12,59 +12,17 @@ var users = require('./routes/users');
 
 var app = express();
 //app.locals.dataset = "4, 8, 15, 16, 23, 42";
-
+var temp_message;
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
-
-//fetching data from server
-var getJSON = function (option, callback) {
-    http.request(option, function (res) {
-        var body = '';
-
-        res.on('data', function (chunk) {
-            body += chunk;
-
-        });
-
-        res.on('end', function () {
-            var price = JSON.parse(body);
-            console.log(body);
-            var temp_array = [];
-            for (var item in price) {
-                temp_array.push(price[item].barvalue)
-            }
-
-            app.locals.dataset = temp_array;
-
-            callback(null, body);
-            //  app.locals.dataset = body;
-        });
-        res.on('error', callback);
-    }).on('error', callback).end();
-
-}
-
-var option = {
-    host: 'demo9745249.mockable.io',
-    port: 80,
-    path: '/linegraphdata',
-    method: 'GET'
-};
-getJSON(option, function (err, result) {
-    if (err) {
-        console.log('error in the callback: ', err);
-    } else {
-        //console.log(result);
-    }
-});
 
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
+
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
@@ -72,12 +30,14 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/users', users);
 
+
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
     var err = new Error('Not Found');
     err.status = 404;
     next(err);
 });
+
 
 // error handler
 app.use(function (err, req, res, next) {
@@ -89,7 +49,6 @@ app.use(function (err, req, res, next) {
     res.status(err.status || 500);
     res.render('error');
 });
-app.get('/', function (req, res) {
-    res.render('index', {fixtureData: fixtureData});
-});
+
+
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,71 @@
 var express = require('express');
 var router = express.Router();
-var d3 = require("d3");
+var http = require('http');
+
+//fetching data from server
+var getJSON = function (option, callback) {
+    http.request(option, function (res) {
+        var body = '';
+
+        res.on('data', function (chunk) {
+            body += chunk;
+
+        });
+
+        res.on('end', function () {
+            price = JSON.parse(body);
+            console.log(price);
+            var temp_array = [];
+            for (var item in price) {
+                temp_array.push(price[item].Year)
+            }
+
+
+            callback(null, price);
+            //  app.locals.dataset = body;
+
+        });
+        res.on('error', callback);
+    }).on('error', callback).end();
+
+}
+
+var weekData = {
+    host: 'demo9745249.mockable.io',
+    port: 80,
+    path: '/weeklydata',
+    method: 'GET'
+};
+var DailyData = {
+    host: 'demo9745249.mockable.io',
+    port: 80,
+    path: '/dailydata',
+    method: 'GET'
+};
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    getJSON(weekData, function (err, result) {
+        if (err) {
+            console.log('error in the callback: ', err);
+        } else {
+            //console.log(result);
+            res.render('index', {data: result});
+        }
+    });
+
+});
+
+router.post('/page', function (req, res) {
+    getJSON(DailyData, function (err, result) {
+        if (err) {
+            console.log('error in the callback: ', err);
+        } else {
+            //console.log(result);
+            res.send(result);
+        }
+    });
+
 });
 
 module.exports = router;
